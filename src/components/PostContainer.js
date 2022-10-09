6cf4a61495e215ef4ff33891c33cd97e5eb2f31f
dev-ahmedali <ahmedali69325@gmail.com>
2022-10-09 23:48:44 +0600

    search functionality

--- src/components/PostContainer.js
+++ src/components/PostContainer.js
@@ -1,29 +1,31 @@
-import React from 'react'
-import { useSelector } from 'react-redux'
-import Filter from './Filter'
-import Header from './Header'
-import Post from './Post'
+import { useSelector } from 'react-redux';
+import Filter from './Filter';
+import Header from './Header';
+import Post from './Post';
 
-export default function PostContainer() {
-    const {filtered_post: posts, filterBy} = useSelector((state) => state)
-    const filter = Object.keys(filterBy)
+
+const PostContainer = () => {
+  const { filtered_post: posts, filterBy } = useSelector((state) => state);
+  const filter = Object.keys(filterBy);
   return (
-    <div className='relative max-w-7x1 mx-auto'>
-        <Header/>
-        {filter.length > 0 && (
-            filter.map((name, index) => {
-                return <Filter key={index} name={name} value={filterBy[name]}/>
-            })
-        )}
+    <div className="relative max-w-7xl mx-auto">
+      <Header />
+     
+      {filter.length > 0 &&
+        filter.map((name, index) => {
+          return <Filter key={index} name={name} value={filterBy[name]} />;
+        })}
+    
+      {posts.length < 1 && (
+        <p className="text-xl font-bold text-red-400">No Post Found.</p>
+      )}
 
-        {posts.length < 1 &&  (
-            <p className="text-xl font-bold text-red-400">No Post Found</p>
-        )}
-         <div className="mt-12 max-w-lg grid gap-5 lg:grid-cols-3 lg:max-w-none">
+      <div className="mt-12 max-w-lg grid gap-5 lg:grid-cols-3 lg:max-w-none">
         {posts.map((post) => {
           return <Post key={post.id} post={post} />;
         })}
       </div>
     </div>
-  )
-}
+  );
+};
+export default PostContainer;
--- src/components/Search.js
+++ src/components/Search.js
@@ -1,6 +1,36 @@
-import React from 'react'
+import React, { useCallback, useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { clearFilter, updateFilter, updateSearch } from '../reudx/posts/actions'
 
 export default function Search() {
+  const {searchText, filterBy} = useSelector((state) => state)
+  const dispatch = useDispatch()
+
+  let debounce = (fn, delay) => {
+    let timerId;
+    return function(searchText) {
+      if(timerId) {
+        clearTimeout(timerId);
+      }
+      timerId = setTimeout(() => {
+        fn(searchText);
+      }, delay)
+    }
+  }
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  debounce = useCallback(debounce((searchText) => {
+    dispatch(updateFilter({name: 'search', value: searchText}))
+  }))
+
+  useEffect(() => {
+    if(searchText) {
+      debounce(searchText)
+    } else {
+      if(filterBy.search) {
+        dispatch(clearFilter())
+      }
+    }
+  }, [searchText, dispatch, filterBy.search])
   return (
     <>
     <input
@@ -8,6 +38,8 @@ export default function Search() {
         type="search"
         name="search"
         placeholder="Search"
+        value={searchText}
+        onChange={(e) => dispatch(updateSearch(e.target.value))}
     />
     <img
         className="inline h-6 cursor-pointer"
